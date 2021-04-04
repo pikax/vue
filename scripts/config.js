c3d153ebf11a322e46736c017d83190944db56e9
Jason <jason.daurus@gmail.com>
2021-04-04 15:57:40 +0800

    fix(test): fix ssr & e2e tests

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const { builtinModules } = require('module')
 const buble = require('rollup-plugin-buble')
-const alias = require('rollup-plugin-alias')
-const cjs = require('rollup-plugin-commonjs')
-const replace = require('rollup-plugin-replace')
-const node = require('rollup-plugin-node-resolve')
+const alias = require('@rollup/plugin-alias')
+const cjs = require('@rollup/plugin-commonjs')
+const replace = require('@rollup/plugin-replace')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
 const ts = require('rollup-plugin-typescript2')
 
 const version = process.env.VERSION || require('../package.json').version
@@ -42,6 +43,7 @@ const builds = {
     entry: resolve('web/entry-runtime.ts'),
     dest: resolve('dist/vue.runtime.common.dev.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'development',
     banner
   },
@@ -49,6 +51,7 @@ const builds = {
     entry: resolve('web/entry-runtime.ts'),
     dest: resolve('dist/vue.runtime.common.prod.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'production',
     banner
   },
@@ -57,6 +60,7 @@ const builds = {
     entry: resolve('web/entry-runtime-with-compiler.ts'),
     dest: resolve('dist/vue.common.dev.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'development',
     alias: { he: './entity-decoder' },
     banner
@@ -65,6 +69,7 @@ const builds = {
     entry: resolve('web/entry-runtime-with-compiler.ts'),
     dest: resolve('dist/vue.common.prod.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'production',
     alias: { he: './entity-decoder' },
     banner
@@ -143,7 +148,8 @@ const builds = {
     entry: resolve('web/entry-compiler.ts'),
     dest: resolve('packages/vue-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-template-compiler/package.json').dependencies)
+    exports: 'auto',
+    external: getExternal(require('../packages/vue-template-compiler/package.json'))
   },
   // Web compiler (UMD for in-browser use).
   'web-compiler-browser': {
@@ -152,22 +158,24 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'VueTemplateCompiler',
-    plugins: [node(), cjs()]
+    plugins: [nodeResolve(), cjs()]
   },
   // Web server renderer (CommonJS).
   'web-server-renderer-dev': {
     entry: resolve('web/entry-server-renderer.ts'),
     dest: resolve('packages/vue-server-renderer/build.dev.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'development',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: getExternal(require('../packages/vue-server-renderer/package.json'))
   },
   'web-server-renderer-prod': {
     entry: resolve('web/entry-server-renderer.ts'),
     dest: resolve('packages/vue-server-renderer/build.prod.js'),
     format: 'cjs',
+    exports: 'auto',
     env: 'production',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    external: getExternal(require('../packages/vue-server-renderer/package.json'))
   },
   'web-server-renderer-basic': {
     entry: resolve('web/entry-server-basic-renderer.ts'),
@@ -175,19 +183,21 @@ const builds = {
     format: 'umd',
     env: 'development',
     moduleName: 'renderVueComponentToString',
-    plugins: [node(), cjs()]
+    plugins: [nodeResolve(), cjs()]
   },
   'web-server-renderer-webpack-server-plugin': {
     entry: resolve('server/webpack-plugin/server.ts'),
     dest: resolve('packages/vue-server-renderer/server-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    exports: 'auto',
+    external: getExternal(require('../packages/vue-server-renderer/package.json'))
   },
   'web-server-renderer-webpack-client-plugin': {
     entry: resolve('server/webpack-plugin/client.ts'),
     dest: resolve('packages/vue-server-renderer/client-plugin.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/vue-server-renderer/package.json').dependencies)
+    exports: 'auto',
+    external: getExternal(require('../packages/vue-server-renderer/package.json'))
   },
   // Weex runtime factory
   'weex-factory': {
@@ -195,6 +205,7 @@ const builds = {
     entry: resolve('weex/entry-runtime-factory.ts'),
     dest: resolve('packages/weex-vue-framework/factory.js'),
     format: 'cjs',
+    exports: 'auto',
     plugins: [weexFactoryPlugin]
   },
   // Weex runtime framework (CommonJS).
@@ -202,7 +213,8 @@ const builds = {
     weex: true,
     entry: resolve('weex/entry-framework.ts'),
     dest: resolve('packages/weex-vue-framework/index.js'),
-    format: 'cjs'
+    format: 'cjs',
+    exports: 'auto',
   },
   // Weex compiler (CommonJS). Used by Weex's Webpack loader.
   'weex-compiler': {
@@ -210,19 +222,39 @@ const builds = {
     entry: resolve('weex/entry-compiler.ts'),
     dest: resolve('packages/weex-template-compiler/build.js'),
     format: 'cjs',
-    external: Object.keys(require('../packages/weex-template-compiler/package.json').dependencies)
+    exports: 'auto',
+    external: getExternal(require('../packages/weex-template-compiler/package.json'))
   }
+}
+
+function getExternal(pkg) {
+  let deps = []
+  if (pkg && pkg.dependencies) {
+    deps = Object.keys(pkg.dependencies)
+  }
+  return [
+    ...builtinModules,
+    ...deps,
+  ]
 }
 
 function genConfig (name) {
   const opts = builds[name]
 
   // console.log('__dir', __dirname)
+  const entries = Object.assign({}, aliases, opts.alias)
+
   const config = {
     input: opts.entry,
     external: opts.external,
     plugins: [
       // flow(),
+      alias({
+        entries: Object.keys(entries).map(name => ({
+          find: name,
+          replacement: entries[name]
+        }))
+      }),
       ts({
         tsconfig: path.resolve(__dirname, '../', 'tsconfig.json'),
         cacheRoot: path.resolve(__dirname, '../','node_modules/.rts2_cache'),
@@ -235,13 +267,13 @@ function genConfig (name) {
           exclude: ['tests', 'test-dts']
         }
       }),
-      alias(Object.assign({}, aliases, opts.alias))
     ].concat(opts.plugins || []),
     output: {
       file: opts.dest,
       format: opts.format,
       banner: opts.banner,
-      name: opts.moduleName || 'Vue'
+      name: opts.moduleName || 'Vue',
+      exports: opts.exports,
     },
     onwarn: (msg, warn) => {
       if (!/Circular/.test(msg)) {
@@ -264,7 +296,10 @@ function genConfig (name) {
   if (opts.env) {
     vars['process.env.NODE_ENV'] = JSON.stringify(opts.env)
   }
-  config.plugins.push(replace(vars))
+  config.plugins.push(replace({
+    preventAssignment: true,
+    values: vars,
+  }))
 
   if (opts.transpile !== false) {
     config.plugins.push(buble())
